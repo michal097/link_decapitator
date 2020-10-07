c23a328ebe5a125ab9e98cf316e7c5177a2c8b4c
michal097 <slusarczyk.michal23@gmail.com>
2020-10-07 22:19:44 +0200

    fix js

---
+++ src/main/resources/static/js/hideNotifications.js
@@ -0,0 +1,10 @@
+function hide() {
+    setTimeout(function(){
+        document.getElementById('inform').classList.add('hidden');
+    },5000)
+}
+function hide1() {
+    setTimeout(function(){
+        document.getElementById('inform1').classList.add('hidden');
+    },5000)
+}
--- src/main/resources/static/js/uploadFile.js
+++
@@ -1,4 +0,0 @@
-$('#uploadfile').bind('change', function() {
-    var fileName = '';
-    fileName = $(this).val();
-    $('#file-selected').html(fileName); })
